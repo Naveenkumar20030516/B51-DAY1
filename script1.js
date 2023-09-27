04cb4c56a9f4a11bf6d90ce55c0ceea97fa56b4e
NAVEEN KUMAR R <142626101+Naveenkumar20030516@users.noreply.github.com>
2023-09-27 12:17:10 +0530

    Add files via upload
    DAY-4 Task

---
+++ script1.js
@@ -0,0 +1,51 @@
+//Resume in JSON format//
+
+let myResume={
+    "basics": {
+      "name": "NAVEEN KUMAR R",
+      "email": "naveenkumar1652003@gamil.com",
+      "phone": 6383463820,
+      "degree": "B.E",
+      "location": {
+        "address": "podanur",
+        "postalCode": 641023,
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/naveen-kumar-r-a78ba525a/",
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "Sri Ramakrishna College Of Engineering",
+        "department": "EEE",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2024,
+        "gpa": 7.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginner",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "football,hockey,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
